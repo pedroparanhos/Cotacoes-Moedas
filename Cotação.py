e7563b964396bfea2d3123ddbdc94b4f98bc0b76
pedroparanhos <pedrolucas817@gmail.com>
2024-05-15 12:06:33 -0300

    Update Cotacao.py

--- Cotação.py
+++ Cotação.py
@@ -35,10 +35,10 @@ class CotacaoApp:
         requisicao = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL,CNY-BRL")
         requisicao_dic = requisicao.json()
 
-        cotacao_dolar = requisicao_dic["USDBRL"]["bid"]
-        cotacao_euro = requisicao_dic["EURBRL"]["bid"]
+        cotacao_dolar = float(requisicao_dic["USDBRL"]["bid"])
+        cotacao_euro = float(requisicao_dic["EURBRL"]["bid"])
         cotacao_btc = float(requisicao_dic["BTCBRL"]["bid"]) * 1000
-        cotacao_yuan = requisicao_dic["CNYBRL"]["bid"]
+        cotacao_yuan = float(requisicao_dic["CNYBRL"]["bid"])
 
         data_atualizacao = datetime.now()
 
